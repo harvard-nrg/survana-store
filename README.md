581858360d37136781fcffa7dc26a83167a2e3af
Victor Petrov <victor.petrov@gmail.com>
2012-11-14 17:42:40 -0500

    Add info to all files

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ survana-save
 ===========
 
 Survana Save Module
+
+Author: Victor Petrov <victor.petrov@gmail.com>
+
+License: New BSD License (see LICENSE file for details).
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,12 @@
+/** routes/index.js
+ *
+ * @author Victor Petrov <victor.petrov@gmail.com>
+ * @copyright (c) 2012, The Neuroinformatics Research Group at Harvard University.
+ * @copyright (c) 2012, The President and Fellows of Harvard College.
+ * @license New BSD License (see LICENSE file for details).
+ */
+
+
 exports.index = function (req, res) {
     "use strict";
 
